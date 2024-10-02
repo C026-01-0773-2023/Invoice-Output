9bf32ea5a5b642f27169774aef9d95cbe695a021
C026-01-0773-2023 <mungai.kiboro23@students.dkut.ac.ke>
2024-10-02 23:09:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+class invoiceOutput {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("************************** C026-01-0773/2023 **************************");
+        System.out.println();
+        System.out.println("#99 ACS Building Gilmore Ave. New Mla.. Quezon City  *  Tel. No: 123-4567");
+        System.out.println("                                              VAT REG. TIN 098-7654321-0000 ");
+        System.out.println("                                                                SALES INVOICE");
+        System.out.println();
+        System.out.println("Sold To: Hillary Mungai                    Date: 02/08/2024");
+        System.out.println("Address : Private bag                        Contact Number: 0100100100");
+        System.out.println();
+        System.out.println("======================================================================");
+        System.out.println("Qty   |    Item Description  |   Unit Price   |   Amount");
+        System.out.println("======================================================================");
+        System.out.println("  4      |     Pencil                      |    20                 |    80.00 ");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("  10    |     Duster                     |    50                 |   500.00 ");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("    9     |     Pens                        |    30                 |    270.00 ");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("   10    |    Crayons                   |    80                 |    800.00 ");
+        System.out.println("=======================================================================");
+        System.out.println("                                                      |Sub Total      |  1650.00");
+        System.out.println("                                                     ================================================");
+        System.out.println("                                                    | VAT(16%)     |  264.00");
+        System.out.println("                                                    ================================================");
+        System.out.println("                                                     |Total Amount |    1914.00");
+        System.out.println("                                                     =================================================");
+
+
+    }
+}
